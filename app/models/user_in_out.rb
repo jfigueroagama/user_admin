17ddfc19c4c8fd582726373e40dc3a2bd450a34b
Josue Figueroa <jfigueroagama@redesvozdatos.com>
2019-05-24 12:27:38 -0500

    User in and out validation fix

--- app/models/user_in_out.rb
+++ app/models/user_in_out.rb
@@ -7,7 +7,7 @@ class UserInOut < ApplicationRecord
   validate :user_out_must_be_greater_than_user_in
 
   def user_out_must_be_greater_than_user_in
-    if user_in >= user_out
+    if user_in != nil && user_out != nil && user_in >= user_out
       errors.add(:user_in, "user_in can't be greater or equal than user_out")
     end
   end
